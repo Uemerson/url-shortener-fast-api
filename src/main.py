3a9a28dbab1c53c1778d56d143a2c64e12e3cbb8
Uemerson Pinheiro Junior <uemersonpinheirojunior@gmail.com>
2022-05-26 23:59:18 -0300

    feat: add forward to target url

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import secrets
 import validators
-from fastapi import Depends, FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException, Request
+from fastapi.responses import RedirectResponse
 from sqlalchemy.orm import Session
 from . import models, schemas
 from .database import SessionLocal, engine
@@ -15,6 +16,11 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+def raise_not_found(request):
+    message = f"URL '{request.url}' doesn't exist"
+    raise HTTPException(status_code=404, detail=message)
 
 
 def raise_bad_request(message):
@@ -42,3 +48,20 @@ def create_url(url: schemas.URLBase, db: Session = Depends(get_db)):
     db_url.url = key
     db_url.admin_url = secret_key
     return db_url
+
+
+@app.get("/{url_key}")
+def forward_to_target_url(
+    url_key: str,
+    request: Request,
+    db: Session = Depends(get_db)
+):
+    db_url = (
+        db.query(models.URL)
+        .filter(models.URL.key == url_key, models.URL.is_active)
+        .first()
+    )
+    if db_url:
+        return RedirectResponse(db_url.target_url)
+    else:
+        raise_not_found(request)
