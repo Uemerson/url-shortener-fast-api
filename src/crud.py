bfb498a5aa24c28469d4f902279cc64b05b3af35
Uemerson Pinheiro Junior <uemersonpinheirojunior@gmail.com>
2022-05-28 14:25:29 -0300

    feat: add get admin info

--- src/crud.py
+++ src/crud.py
@@ -21,3 +21,11 @@ def create_db_url(db: Session, url: schemas.URLBase) -> models.URL:
     db.commit()
     db.refresh(db_url)
     return db_url
+
+
+def get_db_url_by_secret_key(db: Session, secret_key: str) -> models.URL:
+    return (
+        db.query(models.URL)
+        .filter(models.URL.secret_key == secret_key, models.URL.is_active)
+        .first()
+    )
--- src/main.py
+++ src/main.py
@@ -4,6 +4,8 @@ from fastapi.responses import RedirectResponse
 from sqlalchemy.orm import Session
 from . import crud, models, schemas
 from .database import SessionLocal, engine
+from starlette.datastructures import URL
+from .config import get_settings
 
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
@@ -15,6 +17,16 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+def get_admin_info(db_url: models.URL) -> schemas.URLInfo:
+    base_url = URL(get_settings().base_url)
+    admin_endpoint = app.url_path_for(
+        "administration info", secret_key=db_url.secret_key
+    )
+    db_url.url = str(base_url.replace(path=db_url.key))
+    db_url.admin_url = str(base_url.replace(path=admin_endpoint))
+    return db_url
 
 
 def raise_not_found(request):
@@ -36,9 +48,7 @@ def create_url(url: schemas.URLBase, db: Session = Depends(get_db)):
     if not validators.url(url.target_url):
         raise_bad_request(message="Your provided URL is not valid")
     db_url = crud.create_db_url(db=db, url=url)
-    db_url.url = db_url.key
-    db_url.admin_url = db_url.secret_key
-    return db_url
+    return get_admin_info(db_url)
 
 
 @app.get("/{url_key}")
@@ -54,5 +64,19 @@ def forward_to_target_url(
     )
     if db_url := crud.get_db_url_by_key(db=db, url_key=url_key):
         return RedirectResponse(db_url.target_url)
+    else:
+        raise_not_found(request)
+
+
+@app.get(
+    "/admin/{secret_key}",
+    name="administration info",
+    response_model=schemas.URLInfo,
+)
+def get_url_info(
+    secret_key: str, request: Request, db: Session = Depends(get_db)
+):
+    if db_url := crud.get_db_url_by_secret_key(db, secret_key=secret_key):
+        return get_admin_info(db_url)
     else:
         raise_not_found(request)
