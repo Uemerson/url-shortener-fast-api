ebc91d91608b49d4b1c4db114481c1758a366a4b
Uemerson Pinheiro Junior <uemersonpinheirojunior@gmail.com>
2022-05-28 15:56:40 -0300

    feat: add deactivate db url by secret key

--- src/crud.py
+++ src/crud.py
@@ -36,3 +36,12 @@ def update_db_clicks(db: Session, db_url: schemas.URL) -> schemas.URL:
     db.commit()
     db.refresh(db_url)
     return db_url
+
+
+def deactivate_db_url_by_secret_key(db: Session, secret_key: str) -> models.URL:
+    db_url = get_db_url_by_secret_key(db, secret_key)
+    if db_url:
+        db_url.is_active = False
+        db.commit()
+        db.refresh(db_url)
+    return db_url
